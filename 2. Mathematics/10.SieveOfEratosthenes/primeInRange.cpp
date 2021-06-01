d7f96c5ec20ee9e0862b1a4393bae788da6c40dd
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-06-01 06:06:18 +0530

    Mathematics: 9.Divisors 10.Sieve

---
+++ 2. Mathematics/10.SieveOfEratosthenes/primeInRange.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// To print prime from 1 to n. O(n * sqrt n)
+void sieveOfEratosthenes(int n)
+{
+    // init vars
+    vector<bool> isPrime(n + 1, true);
+
+    // logic:
+    for (int i = 2; i * i <= n; i++)
+        if (isPrime[i])
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+
+    for (int i = 2; i <= n; i++)
+        if (isPrime[i])
+            cout << i << " ";
+    // https://www.youtube.com/watch?v=nDPo9hsDNvU
+}
+
+// optimized sieveOfEratosthenes O(n loglog n)
+void optimizedSieveOfEratosthenes(int n)
+{
+    // init vars
+    vector<bool> isPrime(n + 1, true);
+
+    // logic:
+    for (int i = 2; i <= n; i++)
+    {
+
+        if (isPrime[i])
+        {
+            cout << i << " ";
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    // https://www.youtube.com/watch?v=eKp56OLhoQs
+}
+
+int main()
+{
+    int n = 100;
+    sieveOfEratosthenes(n);
+}
---
+++ 2. Mathematics/9.AllDivisiors/allDivisor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Naive solution
+void printDivisors(int n)
+{
+    for (int i = 1; i <= n; i++)
+        if (n % i == 0)
+            cout << i << " ";
+}
+
+// best solution
+void printDivisorEfficiently(int n)
+{
+
+    // logic:
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+            if (i != n / i) // for perfect squares, to prevent printing twice
+                cout << n / i << " ";
+        }
+    }
+}
+
+// Driver code
+int main()
+{
+    int n = 50;
+    printDivisorEfficiently(n);
+}
