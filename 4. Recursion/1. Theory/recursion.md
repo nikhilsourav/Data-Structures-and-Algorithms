0afad5477115ff512bac828925b1debb0b622ae0
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-06-08 11:42:42 +0530

    Recursion: 1.Theory 2.GuessOutput 3.PrintNums

---
+++ 4. Recursion/1. Theory/recursion.md
@@ -0,0 +1,16 @@
+- ## Recursion: function calling itself.
+
+<br></br>
+
+- ## Typical Recursion structure:
+
+  ```
+  ...fun(...){
+      base case
+      ...
+
+      Recursive call (i.e. call to fun())
+      with at least one change in parameter
+  }
+
+  ```
---
+++ 4. Recursion/3. Print Numbers/printNumber.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// head reursion (print 1 to N) Time complexity: O(n), Aux space: O(n)
+void printNumbers(int n)
+{
+    // base case
+    if (n == 0)
+        return;
+
+    // recursion
+    printNumbers(n - 1);
+
+    // print statement
+    cout << n << " ";
+}
+
+// tail recursion (print N to 1) Time complexity: O(n), Aux space: O(n)
+void printNumbersInReverse(int n)
+{
+    // base case
+    if (n == 0)
+        return;
+
+    // print statement
+    cout << n << " ";
+
+    // recursion
+    printNumbersInReverse(n - 1);
+}
+
+// Driver code
+int main()
+{
+    // Given number
+    int num = 10;
+
+    // print 1 to num
+    printNumbers(num);
+    cout << "\n";
+
+    // print num to 1
+    printNumbersInReverse(num);
+}
