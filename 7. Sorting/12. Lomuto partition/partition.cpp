091eaa0178152dd36c5aa7e7a59187d30ed7129f
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-07-06 22:04:47 +0530

    Sorting: 12.Lomuto 13.Hoare

---
+++ 7. Sorting/12. Lomuto partition/partition.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Lumato partition
+int lomutoPartition(int arr[], int low, int high)
+{
+    // init vars
+    int pivot = arr[high]; // assuming pivot is last element
+    int i = low - 1;
+
+    // logic:
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    // insert pivot at correct position
+    swap(arr[i + 1], arr[high]);
+
+    // answer
+    return i + 1;
+}
+
+// Driver code
+int main()
+{
+    // Given array
+    int arr[] = {10, 80, 30, 90, 40, 50, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // fn call
+    lomutoPartition(arr, 0, n - 1);
+
+    // output
+    for (int x : arr)
+        cout << x << " ";
+}
---
+++ 7. Sorting/13. Hoare's partition/partition.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Hoare's partition
+int hoarePartition(int arr[], int low, int high)
+{
+    // init vars
+    int pivot = arr[low]; // assuming pivot is first element
+    int i = low - 1, j = high + 1;
+
+    // logic:
+    while (1)
+    {
+        // increase i
+        do
+        {
+            i++;
+        } while (arr[i] < pivot);
+
+        // decrease j
+        do
+        {
+            j--;
+        } while (arr[j] > pivot);
+
+        // base case
+        if (i >= j)
+            return j;
+
+        // if base case is false swap i and j
+        swap(arr[i], arr[j]);
+    }
+}
+
+// Driver code
+int main()
+{
+    // Given array
+    int arr[] = {5, 3, 8, 4, 2, 7, 1, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // fn call
+    hoarePartition(arr, 0, n - 1);
+
+    // output
+    for (int x : arr)
+        cout << x << " ";
+}
