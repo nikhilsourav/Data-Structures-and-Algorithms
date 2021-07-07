ede713cf1944d4b290aab5063b1acd84bd2c0900
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-07-07 09:18:45 +0530

    Sorting: 14.QuickSortLomuto 15.QuickSortHoare

---
+++ 7. Sorting/14. QuickSort Lomuto/sort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Lumato partition
+int lomutoPartition(int arr[], int low, int high)
+{
+    // init vars
+    int pivot = arr[high]; // assuming pivot is last element
+    int i = low - 1;
+
+    // logic:
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    // insert pivot at correct position
+    swap(arr[i + 1], arr[high]);
+
+    // answer
+    return i + 1;
+}
+
+// Quick sort
+void QuickSort(int arr[], int low, int high)
+{
+    if (high > low)
+    {
+        int pivotIndex = lomutoPartition(arr, low, high);
+        QuickSort(arr, low, pivotIndex - 1);
+        QuickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+// Driver code
+int main()
+{
+    // Given array
+    int arr[] = {8, 4, 7, 9, 3, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // fn call
+    QuickSort(arr, 0, n - 1);
+
+    // output
+    for (int x : arr)
+        cout << x << " ";
+}
---
+++ 7. Sorting/15. QuickSort Hoare/sort.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int hoarePartition(int arr[], int low, int high)
+{
+    // init vars
+    int pivot = arr[low]; // assuming pivot is first element
+    int i = low - 1, j = high + 1;
+
+    // logic:
+    while (1)
+    {
+        // increase i
+        do
+        {
+            i++;
+        } while (arr[i] < pivot);
+
+        // decrease j
+        do
+        {
+            j--;
+        } while (arr[j] > pivot);
+
+        // base case
+        if (i >= j)
+            return j;
+
+        // if base case is false swap i and j
+        swap(arr[i], arr[j]);
+    }
+}
+
+// QuickSort
+void QuickSort(int arr[], int low, int high)
+{
+    if (high > low)
+    {
+        int pivotIndex = hoarePartition(arr, low, high);
+        QuickSort(arr, low, pivotIndex);
+        QuickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+// Driver code
+int main()
+{
+    // Given array
+    int arr[] = {8, 4, 7, 9, 3, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // fn call
+    QuickSort(arr, 0, n - 1);
+
+    // output
+    for (int x : arr)
+        cout << x << " ";
+}
