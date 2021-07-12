2353d3febad3ec2d70c3be7e6533984fc79c50c5
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-07-12 09:05:17 +0530

    Matrix: 5.Transpose 6.Rotate

---
+++ 8. Matrix/6. Rotate/rotate.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// global const
+const int SIZE = 4;
+
+// utility fn to print
+void print(int matrix[SIZE][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+            cout << matrix[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+// rotate 90 deg anticlockwise (transpose and reverse each column)
+void rotateLeft(int matrix[SIZE][SIZE])
+{
+    // transpose
+    for (int i = 0; i < SIZE; i++)
+        for (int j = i + 1; j < SIZE; j++)
+            swap(matrix[i][j], matrix[j][i]);
+
+    // reverse
+    for (int i = 0; i < SIZE; i++)
+    {
+        int low = 0, high = SIZE - 1;
+
+        while (low < high)
+        {
+            swap(matrix[low][i], matrix[high][i]);
+
+            low++;
+            high--;
+        }
+    }
+}
+
+// rotate 90 deg clockwise (transpose and reverse each row)
+void rotateRight(int matrix[SIZE][SIZE])
+{
+    // transpose
+    for (int i = 0; i < SIZE; i++)
+        for (int j = i + 1; j < SIZE; j++)
+            swap(matrix[i][j], matrix[j][i]);
+
+    // reverse
+    for (int i = 0; i < SIZE; i++)
+    {
+        int low = 0, high = SIZE - 1;
+
+        while (low < high)
+        {
+            swap(matrix[i][low], matrix[i][high]);
+
+            low++;
+            high--;
+        }
+    }
+}
+
+// Driver code
+int main()
+{
+    // given matrix
+    int matrix[SIZE][SIZE] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+    // print given matrix
+    cout << "Given matrix:\n";
+    print(matrix);
+
+    // fn call
+    rotateLeft(matrix);
+
+    // print output
+    cout << "\nRotated 90 deg anti-clockwise:\n";
+    print(matrix);
+
+    // fn call
+    rotateRight(matrix); // counter first rotation
+    rotateRight(matrix); // rotate right given matrix
+
+    // print output
+    cout << "\nRotated 90 deg clockwise:\n";
+    print(matrix);
+}
