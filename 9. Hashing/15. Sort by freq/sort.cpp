a86a7f402c6fe8e555049fe74afefdd2edc30e7e
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-07-22 15:51:19 +0530

    Hashing: 15.SortByFreq

---
+++ 9. Hashing/15. Sort by freq/sort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// sort array by frequency
+vector<int> sortByFreq(int arr[], int n)
+{
+    // create map
+    unordered_map<int, int> freq;
+
+    // store frequency
+    for (int i = 0; i < n; i++)
+        freq[arr[i]]++;
+
+    // vect of pair
+    vector<pair<int, int>> vectPair;
+
+    // store map elements in vector
+    for (auto &val : freq)
+        vectPair.push_back(val);
+
+    // sort
+    sort(begin(vectPair), end(vectPair), [&](pair<int, int> a, pair<int, int> b)
+         { return a.second != b.second ? a.second > b.second : a.first < b.first; });
+
+    // export answer
+    vector<int> result;
+    for (auto &val : vectPair)
+        while (val.second--)
+            result.push_back(val.first);
+
+    // return
+    return result;
+}
+
+int main()
+{
+    // Given array
+    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Output
+    vector<int> ans = sortByFreq(arr, n);
+    for (auto x : ans)
+        cout << x << " ";
+}
