19238516235445c6f3cbaf607e652d3a999ce437
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-07-23 07:10:18 +0530

    Hashing: 17.GroupAnagram

---
+++ 9. Hashing/17. Group Anagram/anagram.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Group anagrams together
+vector<vector<string>> Anagrams(vector<string> &strs)
+{
+    // anwser vector
+    vector<vector<string>> ans;
+
+    // size of given vector
+    int n = strs.size();
+
+    // if given vector is empty
+    if (n == 0)
+        return ans;
+
+    // create map
+    unordered_map<string, vector<string>> m;
+
+    // sort each string
+    for (auto i : strs)
+    {
+        string s = i;
+        sort(s.begin(), s.end());
+        m[s].push_back(i);
+    }
+
+    // push in map
+    for (auto i : m)
+        ans.push_back(i.second);
+
+    // answer
+    return ans;
+}
+
+// Driver code
+int main()
+{
+    // Given string
+    vector<string> words = {"act", "god", "cat", "dog", "tac"};
+
+    // store ans
+    vector<vector<string>> ans = Anagrams(words);
+
+    // print answer
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+            cout << ans[i][j] << " ";
+        cout << "\n";
+    }
+}
