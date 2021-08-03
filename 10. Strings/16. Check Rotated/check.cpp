8334a1431b213c53592fde22a9bb19de20e16150
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-08-03 07:20:28 +0530

    Strings: 16.CheckRotated

---
+++ 10. Strings/16. Check Rotated/check.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// check if two strings are rotation of each other
+bool areRotations(string s1, string s2)
+{
+    return (s1.length() == s2.length()) && ((s1 + s1).find(s2) != string::npos);
+}
+
+int main()
+{
+    // Given string
+    string s1 = "ABCD", s2 = "CDAB";
+
+    // fn call
+    cout << boolalpha << areRotations(s1, s2);
+}
