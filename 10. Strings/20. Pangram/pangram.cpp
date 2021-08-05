64bea1b2f2cde53edb720fe7d85bbb990c3657e9
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-08-05 21:48:38 +0530

    Strings: 20.Pangram

---
+++ 10. Strings/20. Pangram/pangram.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+const int CHAR = 256;
+
+// pangram without using set
+bool checkPangram(string &str)
+{
+    //initialize an array of 256 chars with all elements as 0
+    int mark[CHAR] = {};
+
+    // init index and size
+    int index = 0, n = str.size();
+
+    // logic
+    for (int i = 0; i < n; i++)
+    {
+        // increment mark for each uppercase character
+        if (str[i] >= 65 && str[i] <= 90)
+        {
+            index = str[i] - 65;
+            mark[index]++;
+        }
+        // increment mark for each lowercase character
+        else if (str[i] >= 97 && str[i] <= 122)
+        {
+            index = str[i] - 97;
+            mark[index]++;
+        }
+    }
+
+    // if any mark value is 0 that means a char is missing from a-z
+    for (int i = 0; i < 26; i++)
+        if (mark[i] == 0)
+            return false;
+
+    // if pangram
+    return true;
+}
+
+// Driver code
+int main()
+{
+    // Given string
+    string s = "Bawds jog, flick quartz, vex nymph";
+
+    // fn call
+    cout << boolalpha << checkPangram(s);
+}
