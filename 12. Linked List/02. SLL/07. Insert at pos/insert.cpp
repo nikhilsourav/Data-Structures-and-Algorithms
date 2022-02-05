4c61f00103d232dd62c3e005192ac97f901f5158
Nikhil Sourav <nikhil.rj900@gmail.com>
2022-02-05 15:52:40 +0530

    minor fix

--- 12. Linked List/02. SLL/07. Insert at pos/insert.cpp
+++ 12. Linked List/02. SLL/07. Insert at pos/insert.cpp
@@ -1,6 +1,11 @@
 /*
     Insert an item at a given position in the linked list
-    eg: list = [1->2->3->4], item = 5, pos = 2 => [1->5->2->3->4]
+    The item's index should be the given index (pos) after insertion
+
+    list = [1->2->3->4],
+    item = 5, pos = 1 (0-based indexing)
+
+    output: [1->5->2->3->4]
 */
 
 #include <bits/stdc++.h>
@@ -19,14 +24,28 @@ public:
     }
 };
 
-// Insert
+/*
+    Case 1: If pos == 0
+        Create newNode with val, newNode->next = head
+
+    Case 2: If pos > 1
+        Start from i = 0 and reach one node before pos node
+        Note that the loop will run from [0 to pos - 1)
+        
+        for(i = 0; i < pos - 1 and head != NULL; i++)
+            prt = prt->next;
+
+        Rearrange links
+*/
+
+// Insert at position
 Node *insertPos(Node *head, int pos, int val)
 {
     // allocate new node in heap
     Node *newNode = new Node(val);
 
-    // if position is 1
-    if (pos == 1)
+    // if position is 0
+    if (pos == 0)
     {
         newNode->next = head;
         return newNode;
@@ -35,8 +54,8 @@ Node *insertPos(Node *head, int pos, int val)
     // for general case init ptr to traverse list
     Node *ptr = head;
 
-    // reach pos by updateing ptr
-    for (int i = 1; i <= pos - 2 && ptr != NULL; i++)
+    // reach pos by updating ptr
+    for (int i = 0; i < pos - 1 && ptr != NULL; i++)
         ptr = ptr->next;
 
     // if pos cross list size => ptr == NULL => do nothing
@@ -45,7 +64,6 @@ Node *insertPos(Node *head, int pos, int val)
 
     // link new node to ptr->next
     newNode->next = ptr->next;
-
     // link ptr to new node
     ptr->next = newNode;
 
@@ -77,7 +95,7 @@ int main()
     // Print given list
     printList(head);
 
-    // Insert at 2nd position
+    // Insert at 2nd position (index = 1)
     head = insertPos(head, 2, 50);
 
     // Print after insertion
